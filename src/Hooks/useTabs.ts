8ad4e26bbbd415987b82cf6838b55edf81c5108c
plusbeauxjours <plusbeauxjours@gmail.com>
2019-09-05 13:29:09 +0900

    daily commit

--- src/Hooks/useTabs.ts
+++ src/Hooks/useTabs.ts
@@ -1,9 +1,11 @@
 import { useState } from "react";
 
+export const content = [
+  { tab: "Section 1", content: "I'm the content fof the Section 1" },
+  { tab: "Section 2", content: "I'm the content fof the Section 2" }
+];
+
 const useTabs = (initialTab: any, allTabs: any) => {
-  if (!allTabs || !Array.isArray(allTabs)) {
-    return;
-  }
   const [currentIndex, setCurrentIndex] = useState(initialTab);
   return {
     currentItem: allTabs[currentIndex],
--- src/Routes/Input.tsx
+++ src/Routes/Input.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import useInput from "../Hooks/useInput";
+import useTabs from "../Hooks/useTabs";
+import { content } from "../Hooks/useTabs";
 
 const Container = styled.div`
   display: flex;
@@ -10,12 +12,27 @@ const Container = styled.div`
   margin-top: 300px;
 `;
 
+const BtnContainer = styled.div`
+  display: flex;
+  margin: 10px 0;
+`;
+const Btn = styled.button``;
+
 const Input = () => {
   const noAt = (value: any) => !value.includes("@");
   const name = useInput("Mr.", noAt);
+  const { currentItem, changeItem } = useTabs(0, content);
   return (
     <Container>
       <input placeholder="Name" {...name} />
+      <BtnContainer>
+        {content.map((section, index) => (
+          <Btn key={index} onClick={() => changeItem(index)}>
+            {section.tab}
+          </Btn>
+        ))}
+      </BtnContainer>
+      <div>{currentItem.content}</div>
     </Container>
   );
 };
